7dc2062b88a8f98ded003ab66d594df537df17be
Sadik-Sami <miasimanto@gmail.com>
2025-11-10 09:11:53 +0600

    Initial commit

---
+++ Server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	controller "github.com/Sadik-Sami/MagicStreamMovies/Server/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMovies")
+	})
+
+	router.GET("/movies", controller.GetMovies())
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
+}
